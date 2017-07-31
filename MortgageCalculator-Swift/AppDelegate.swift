75d994c0eb13bb0b1beaf678d7a23635a9538e86
xuzhou524 <xuuohz@163.com>
2017-07-31 11:25:54 +0800

    添加Root

--- MortgageCalculator-Swift/AppDelegate.swift
+++ MortgageCalculator-Swift/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow()
+        self.window?.frame = UIScreen.main.bounds
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = RootViewController()
+        
         return true
     }
 
---
+++ MortgageCalculator-Swift/RootViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RootViewController.swift
+//  MortgageCalculator-Swift
+//
+//  Created by gozap on 2017/7/31.
+//  Copyright © 2017年 com.longdai. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray;
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
