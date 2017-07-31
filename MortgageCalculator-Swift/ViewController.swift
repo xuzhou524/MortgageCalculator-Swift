19a88d8d4d2627114ef37038e7e89e33c39e6ff2
xuzhou524 <xuuohz@163.com>
2017-07-31 11:00:34 +0800

    first commit

---
+++ MortgageCalculator-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MortgageCalculator-Swift
+//
+//  Created by gozap on 2017/7/31.
+//  Copyright © 2017年 com.longdai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
