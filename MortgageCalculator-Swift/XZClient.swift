4d1cd6d52359c0706edbb8ad8697d30356c2cf4e
xuzhou524 <xuuohz@163.com>
2017-08-02 15:59:06 +0800

    bak

---
+++ MortgageCalculator-Swift/XZClient.swift
@@ -0,0 +1,24 @@
+//
+//  XZClient.swift
+//  MortgageCalculator-Swift
+//
+//  Created by gozap on 2017/8/2.
+//  Copyright © 2017年 com.longdai. All rights reserved.
+//
+
+import UIKit
+
+class XZClient: NSObject{
+    
+    static var screenWidth : CGFloat{
+        get{
+            return UIScreen.main.bounds.width
+        }
+    }
+    
+    static var screenHeight : CGFloat{
+        get{
+            return UIScreen.main.bounds.height
+        }
+    }
+}
