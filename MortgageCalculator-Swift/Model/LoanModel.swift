f860e1c10b4c441a1029994f4658153885462171
xuzhou524 <xuuohz@163.com>
2017-08-08 17:42:59 +0800

    添加每月应还利息

--- MortgageCalculator-Swift/Model/LoanModel.swift
+++ MortgageCalculator-Swift/Model/LoanModel.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class LoanModel: NSObject {
-    class func monthlyMonthSupply(principal:CGFloat , monthRate:CGFloat , periods:Int) -> CGFloat{
+    // principal     贷款本金
+    // monthRate     月利率
+    // totalMonths   还款月数
+    // periods       还款月序号
+    //每月月供额
+    class func averageCapitalPlusInterestMonthAmount(principal:CGFloat , monthRate:CGFloat , totalMonths:Int) -> CGFloat{
         //每月月供额=〔贷款本金×月利率×(1＋月利率)＾还款月数〕÷〔(1＋月利率)＾还款月数-1〕
+        return UIScreen.main.bounds.width
+    }
+    
+    //每月应还利息
+    class func averageCapitalPlusInterestMonthInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:Int , periods:Int) -> CGFloat{
+        //每月应还利息=贷款本金×月利率×〔(1+月利率)^还款月数-(1+月利率)^(还款月序号-1)〕÷〔(1+月利率)^还款月数-1〕
         return UIScreen.main.bounds.width
     }
 }
