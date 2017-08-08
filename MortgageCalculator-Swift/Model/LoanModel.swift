ea48c91d2b7f8c8e144dba2aeee8e1bca6e20ca6
xuzhou524 <xuuohz@163.com>
2017-08-08 17:33:38 +0800

    添加  计算每月月供额

---
+++ MortgageCalculator-Swift/Model/LoanModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoanModel.swift
+//  MortgageCalculator-Swift
+//
+//  Created by gozap on 2017/8/8.
+//  Copyright © 2017年 com.longdai. All rights reserved.
+//
+
+import UIKit
+
+class LoanModel: NSObject {
+    class func monthlyMonthSupply(principal:CGFloat , monthRate:CGFloat , periods:Int) -> CGFloat{
+        //每月月供额=〔贷款本金×月利率×(1＋月利率)＾还款月数〕÷〔(1＋月利率)＾还款月数-1〕
+        return UIScreen.main.bounds.width
+    }
+}
