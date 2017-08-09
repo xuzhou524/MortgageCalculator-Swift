5d7e96da8158db3b6e36f8867bdad754a089f686
xuzhou524 <xuuohz@163.com>
2017-08-09 09:31:26 +0800

    完善 等额本息  每月本息

--- MortgageCalculator-Swift/Model/LoanModel.swift
+++ MortgageCalculator-Swift/Model/LoanModel.swift
@@ -20,25 +20,25 @@ class LoanModel: NSObject {
     */
     
     //每月月供额
-    class func averageCapitalPlusInterestMonthAmount(principal:CGFloat , monthRate:CGFloat , totalMonths:Int) -> CGFloat{
+    class func averageCapitalPlusInterestMonthAmount(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont) -> CGFloat{
         //每月月供额 =〔贷款本金 × 月利率 × (1＋月利率)＾还款月数〕/〔(1＋月利率)＾还款月数-1〕
-        return UIScreen.main.bounds.width
+        return (principal * monthRate * pow(1+monthRate,totalMonths as! CGFloat)/(pow(1+monthRate,totalMonths as! CGFloat)-1))
     }
     
     //每月应还利息
-    class func averageCapitalPlusInterestMonthInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:Int , numberMonth:Int) -> CGFloat{
+    class func averageCapitalPlusInterestMonthInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont , numberMonth:CGFont) -> CGFloat{
         //每月应还利息 = 贷款本金 × 月利率×〔(1+月利率)^还款月数 - (1+月利率)^(还款月序号-1)〕/〔(1+月利率)^还款月数-1〕
         return UIScreen.main.bounds.width
     }
     
     //每月应还本金
-    class func averageCapitalPlusInterestMonthPrincipal(principal:CGFloat , monthRate:CGFloat , totalMonths:Int , numberMonth:Int) -> CGFloat{
+    class func averageCapitalPlusInterestMonthPrincipal(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont , numberMonth:CGFont) -> CGFloat{
         //每月应还本金 = 贷款本金 × 月利率 × (1+月利率)^(还款月序号 - 1) /〔(1+月利率)^还款月数-1〕
         return UIScreen.main.bounds.width
     }
     
     //总利息
-    class func averageCapitalPlusInterestTotalInterest(principal:CGFloat , totalMonths:Int , monthAmount:CGFloat) -> CGFloat{
+    class func averageCapitalPlusInterestTotalInterest(principal:CGFloat , totalMonths:CGFont , monthAmount:CGFloat) -> CGFloat{
         //总利息 = 还款月数 × 每月月供额 - 贷款本金
         return UIScreen.main.bounds.width
     }
@@ -49,31 +49,31 @@ class LoanModel: NSObject {
      */
     
     //每月月供额
-    class func equalPrincipalMonthAmount(principal:CGFloat , monthRate:CGFloat , totalMonths:Int) -> CGFloat{
+    class func equalPrincipalMonthAmount(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont) -> CGFloat{
         //每月月供额 = (贷款本金 / 还款月数) + (贷款本金 - 已归还本金累计额) × 月利率
         return UIScreen.main.bounds.width
     }
     
     //每月应还利息
-    class func equalPrincipalMonthInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:Int , numberMonth:Int) -> CGFloat{
+    class func equalPrincipalMonthInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont , numberMonth:CGFont) -> CGFloat{
         //每月应还利息 = 剩余本金×月利率 = (贷款本金 - 已归还本金累计额) × 月利率
         return UIScreen.main.bounds.width
     }
     
     //每月应还本金
-    class func equalPrincipalMonthPrincipal(principal:CGFloat , totalMonths:Int) -> CGFloat{
+    class func equalPrincipalMonthPrincipal(principal:CGFloat , totalMonths:CGFont) -> CGFloat{
         //每月应还本金 = 贷款本金 / 还款月数
         return UIScreen.main.bounds.width
     }
     
     //总利息
-    class func equalPrincipalTotalInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:Int , monthAmount:CGFloat) -> CGFloat{
+    class func equalPrincipalTotalInterest(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont , monthAmount:CGFloat) -> CGFloat{
         //总利息 =〔(贷款本金 / 还款月数 + 贷款本金 × 月利率) + 贷款本金 / 还款月数 × (1+月利率)〕/ 2×还款月数 - 贷款本金
         return UIScreen.main.bounds.width
     }
     
     //每月月供递减额
-    class func equalPrincipalMonthlyDecreaseInMonthlySupply(principal:CGFloat , monthRate:CGFloat , totalMonths:Int) -> CGFloat{
+    class func equalPrincipalMonthlyDecreaseInMonthlySupply(principal:CGFloat , monthRate:CGFloat , totalMonths:CGFont) -> CGFloat{
         //每月月供递减额 = 贷款本金 / 还款月数 × 月利率
         return UIScreen.main.bounds.width
     }
