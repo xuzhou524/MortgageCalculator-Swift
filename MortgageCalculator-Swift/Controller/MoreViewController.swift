931294c66109688b38c9b43b2084091747eadb53
xuzhou524 <xuuohz@163.com>
2017-08-11 08:21:10 +0800

    清缓存

--- MortgageCalculator-Swift/Controller/MoreViewController.swift
+++ MortgageCalculator-Swift/Controller/MoreViewController.swift
@@ -76,7 +76,7 @@ class MoreViewController: UITableViewController {
         }
         if (indexPath as NSIndexPath).row == 5 {
             baseCell.titleLabel?.text = "清除缓存"
-            baseCell.detaileLabel?.text = ""
+            baseCell.detaileLabel?.text = String(self.fileSizeOfCache()) + "  M"
         }
         if (indexPath as NSIndexPath).row == 6 {
             let infoDict = Bundle.main.infoDictionary
@@ -91,7 +91,51 @@ class MoreViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        if indexPath.row == 5 {
+            self.clearCache()
+        }
+    }
+    
+    func fileSizeOfCache()-> Int {
+        // 取出cache文件夹目录 缓存文件都在这个目录下
+        let cachePath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).first
+        //缓存目录路径
+        // 取出文件夹下所有文件数组
+        let fileArr = FileManager.default.subpaths(atPath: cachePath!)
+        //快速枚举出所有文件名 计算文件大小
+        var size = 0
+        for file in fileArr! {
+            // 把文件名拼接到路径中
+            let path = (cachePath! as NSString).appending("/\(file)")
+            // 取出文件属性
+            let floder = try! FileManager.default.attributesOfItem(atPath: path)
+            // 用元组取出文件大小属性
+            for (abc, bcd) in floder {
+                // 累加文件大小
+                if abc == FileAttributeKey.size {
+                    size += (bcd as AnyObject).integerValue
+                }
+            }
+        }
+        let mm = size / 1024 / 1024
+        return mm
+    }
+    func clearCache() {
+        // 取出cache文件夹目录 缓存文件都在这个目录下
+        let cachePath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).first
+        // 取出文件夹下所有文件数组
+        let fileArr = FileManager.default.subpaths(atPath: cachePath!)
+        // 遍历删除
+        for file in fileArr! {
+            let path = (cachePath! as NSString).appending("/\(file)")
+            if FileManager.default.fileExists(atPath: path) {
+                do {
+                    try FileManager.default.removeItem(atPath: path)
+                } catch {
+                }
+            }
+        }
+        self.tableView.reloadData()
     }
 
     func zanImageViewTap(){
