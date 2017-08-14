c956a72d6d12b8233ee36aa00d77d853f32ee412
xuzhou524 <xuuohz@163.com>
2017-08-14 10:38:41 +0800

    结果详情

--- MortgageCalculator-Swift/Controller/LoanDetailsTableViewController.swift
+++ MortgageCalculator-Swift/Controller/LoanDetailsTableViewController.swift
@@ -18,8 +18,13 @@ class LoanDetailsTableViewController: UITableViewController {
         super.viewDidLoad()
 
         self.navigationItem.title = "结算结果"
-        self.tableView.backgroundColor = XZSwiftColor.convenientBackgroundColor
+        self.tableView.backgroundColor = XZSwiftColor.white
         self.tableView.separatorStyle = .none
+        
+        regClass(self.tableView, cell: LoanDetails_TitleTableViewCell.self)
+        regClass(self.tableView, cell: LoanDetails_DescribeTableViewCell.self)
+        regClass(self.tableView, cell: LoanDetails_CumulativeTableViewCell.self)
+        regClass(self.tableView, cell: LoanDetailsTableViewCell.self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,70 +33,78 @@ class LoanDetailsTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if section == 0 {
+            return 3
+        }
+        return 30
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                return 60
+            case 1:
+                return 80
+            case 2:
+                return 80
+            default: break
+            }
+        }else{
+            return 45
+        }
         return 0
     }
-
-    /*
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 1 {
+            return 40
+        }
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headView = UIView()
+        headView.clipsToBounds = true
+        headView.backgroundColor = XZSwiftColor.convenientBackgroundColor
+        
+        let loandTitleView = LoanDetailsTitleTableView()
+        headView.addSubview(loandTitleView)
+        loandTitleView.snp.makeConstraints { (make) in
+            make.center.equalTo(headView)
+            make.width.equalTo(XZClient.ScreenWidth())
+            make.height.equalTo(40)
+        }
+        return headView
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                let cell = getCell(tableView, cell: LoanDetails_TitleTableViewCell.self, indexPath: indexPath)
+                return cell
+            case 1:
+                let cell = getCell(tableView, cell: LoanDetails_DescribeTableViewCell.self, indexPath: indexPath)
+                return cell
+            case 2:
+                let cell = getCell(tableView, cell: LoanDetails_CumulativeTableViewCell.self, indexPath: indexPath)
+                return cell
+            default: break
+            }
+        }else{
+            let cell = getCell(tableView, cell: LoanDetailsTableViewCell.self, indexPath: indexPath)
+            return cell
+        }
+        return UITableViewCell()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
--- MortgageCalculator-Swift/View/LoanDetailsTableViewCell.swift
+++ MortgageCalculator-Swift/View/LoanDetailsTableViewCell.swift
@@ -26,11 +26,10 @@ class LoanDetailsTableViewCell: UITableViewCell {
     }
     
     func sebView(){
-        self.contentView.backgroundColor = XZSwiftColor.convenientBackgroundColor
+        self.contentView.backgroundColor = XZSwiftColor.white
         self.periodLabel = UILabel()
-        self.periodLabel?.text = "期数"
+        self.periodLabel?.text = "第一期"
         self.periodLabel?.font = XZClient.XZFont2(size: 14)
-        self.periodLabel?.textColor = XZSwiftColor.xzGlay129
         self.periodLabel?.textAlignment = .center
         self.contentView.addSubview(self.periodLabel!)
         self.periodLabel?.snp.makeConstraints({ (make) -> Void in
@@ -40,9 +39,8 @@ class LoanDetailsTableViewCell: UITableViewCell {
         });
         
         self.monthAmountLabel = UILabel()
-        self.monthAmountLabel?.text = "月供（元）"
+        self.monthAmountLabel?.text = "3568.72"
         self.monthAmountLabel?.font = XZClient.XZFont2(size: 14)
-        self.monthAmountLabel?.textColor = XZSwiftColor.xzGlay129
         self.monthAmountLabel?.textAlignment = .center
         self.contentView.addSubview(self.monthAmountLabel!)
         self.monthAmountLabel?.snp.makeConstraints({ (make) -> Void in
@@ -52,9 +50,8 @@ class LoanDetailsTableViewCell: UITableViewCell {
         });
         
         self.principalLabel = UILabel()
-        self.principalLabel?.text = "本金（元）"
+        self.principalLabel?.text = "989.54"
         self.principalLabel?.font = XZClient.XZFont2(size: 14)
-        self.principalLabel?.textColor = XZSwiftColor.xzGlay129
         self.principalLabel?.textAlignment = .center
         self.contentView.addSubview(self.principalLabel!)
         self.principalLabel?.snp.makeConstraints({ (make) -> Void in
@@ -63,18 +60,91 @@ class LoanDetailsTableViewCell: UITableViewCell {
             make.width.equalTo(XZClient.ScreenWidth()/4.0)
         });
         
-        self.principalLabel = UILabel()
-        self.principalLabel?.text = "利息（元）"
-        self.principalLabel?.font = XZClient.XZFont2(size: 14)
-        self.principalLabel?.textColor = XZSwiftColor.xzGlay129
-        self.principalLabel?.textAlignment = .center
-        self.contentView.addSubview(self.principalLabel!)
-        self.principalLabel?.snp.makeConstraints({ (make) -> Void in
+        self.interestLabel = UILabel()
+        self.interestLabel?.text = "1354.21"
+        self.interestLabel?.font = XZClient.XZFont2(size: 14)
+        self.interestLabel?.textAlignment = .center
+        self.contentView.addSubview(self.interestLabel!)
+        self.interestLabel?.snp.makeConstraints({ (make) -> Void in
             make.centerY.equalTo(self.contentView)
-            make.right.equalTo((self.monthAmountLabel?.snp.right)!)
+            make.left.equalTo((self.principalLabel?.snp.right)!)
             make.width.equalTo(XZClient.ScreenWidth()/4.0)
         });
         
+    }
+}
+
+
+class LoanDetailsTitleTableView: UIView {
+    
+    var periodLabel: UILabel?
+    var monthAmountLabel: UILabel?
+    var principalLabel: UILabel?
+    var interestLabel: UILabel?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.sebView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.sebView()
+    }
+    
+    func sebView(){
+
+        self.periodLabel = UILabel()
+        self.periodLabel?.text = "期数"
+        self.periodLabel?.font = XZClient.XZFont2(size: 14)
+        self.periodLabel?.textAlignment = .center
+        self.addSubview(self.periodLabel!)
+        self.periodLabel?.snp.makeConstraints({ (make) -> Void in
+            make.centerY.equalTo(self)
+            make.left.equalTo(self)
+            make.width.equalTo(XZClient.ScreenWidth()/4.0)
+        });
+        
+        self.monthAmountLabel = UILabel()
+        self.monthAmountLabel?.text = "月供（元）"
+        self.monthAmountLabel?.font = XZClient.XZFont2(size: 14)
+        self.monthAmountLabel?.textAlignment = .center
+        self.addSubview(self.monthAmountLabel!)
+        self.monthAmountLabel?.snp.makeConstraints({ (make) -> Void in
+            make.centerY.equalTo(self)
+            make.left.equalTo((self.periodLabel?.snp.right)!)
+            make.width.equalTo(XZClient.ScreenWidth()/4.0)
+        });
+        
+        self.principalLabel = UILabel()
+        self.principalLabel?.text = "本金（元）"
+        self.principalLabel?.font = XZClient.XZFont2(size: 14)
+        self.principalLabel?.textAlignment = .center
+        self.addSubview(self.principalLabel!)
+        self.principalLabel?.snp.makeConstraints({ (make) -> Void in
+            make.centerY.equalTo(self)
+            make.left.equalTo((self.monthAmountLabel?.snp.right)!)
+            make.width.equalTo(XZClient.ScreenWidth()/4.0)
+        });
+        
+        self.interestLabel = UILabel()
+        self.interestLabel?.text = "利息（元）"
+        self.interestLabel?.font = XZClient.XZFont2(size: 14)
+        self.interestLabel?.textAlignment = .center
+        self.addSubview(self.interestLabel!)
+        self.interestLabel?.snp.makeConstraints({ (make) -> Void in
+            make.centerY.equalTo(self)
+            make.left.equalTo((self.principalLabel?.snp.right)!)
+            make.width.equalTo(XZClient.ScreenWidth()/4.0)
+        });
+        
+        let linBView = UIView()
+        linBView.backgroundColor = XZSwiftColor.xzGlay230
+        self.addSubview(linBView)
+        linBView.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(0.5);
+            make.left.bottom.right.equalTo(self)
+        }
     }
 }
 
@@ -94,42 +164,38 @@ class LoanDetails_TitleTableViewCell: UITableViewCell {
     }
     
     func sebView(){
-        self.contentView.backgroundColor = XZSwiftColor.convenientBackgroundColor
+        self.contentView.backgroundColor = XZSwiftColor.white
         
         self.titleLabel = UILabel()
         self.titleLabel?.text = "每月月供"
         self.titleLabel?.font = XZClient.XZFont2(size: 14)
-        self.titleLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.titleLabel!)
         self.titleLabel?.snp.makeConstraints({ (make) -> Void in
-            make.bottom.equalTo(self.contentView.snp.centerY).offset(-5)
+            make.top.equalTo(self.contentView).offset(15)
             make.left.equalTo(self.contentView).offset(15)
         });
         
         let label = UILabel()
         label.text = "¥"
-        label.font = XZClient.XZFont2(size: 18)
-        label.textColor = XZSwiftColor.xzGlay129
+        label.font = XZClient.XZFont2(size: 20)
         self.contentView.addSubview(label)
         label.snp.makeConstraints({ (make) -> Void in
-            make.top.equalTo(self.contentView.snp.centerY).offset(5)
+            make.top.equalTo((self.titleLabel?.snp.bottom)!).offset(13)
             make.left.equalTo(self.titleLabel!)
         });
         
         self.amountLabel = UILabel()
         self.amountLabel?.text = "5647.64"
-        self.amountLabel?.font = XZClient.XZFont2(size: 18)
-        self.amountLabel?.textColor = XZSwiftColor.xzGlay129
+        self.amountLabel?.font = XZClient.XZFont2(size: 22)
         self.contentView.addSubview(self.amountLabel!)
         self.amountLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(label)
-            make.left.equalTo(label.snp.right).offset(5)
+            make.left.equalTo(label.snp.right).offset(3)
         });
         
         let unitLabel = UILabel()
         unitLabel.text = "元"
         unitLabel.font = XZClient.XZFont2(size: 14)
-        unitLabel.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(unitLabel)
         unitLabel.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(self.amountLabel!)
@@ -160,42 +226,38 @@ class LoanDetails_DescribeTableViewCell: UITableViewCell {
     }
     
     func sebView(){
-        self.contentView.backgroundColor = XZSwiftColor.convenientBackgroundColor
+        self.contentView.backgroundColor = XZSwiftColor.white
         
         self.totalAmountTitleLabel = UILabel()
         self.totalAmountTitleLabel?.text = "贷款总额"
         self.totalAmountTitleLabel?.font = XZClient.XZFont2(size: 14)
-        self.totalAmountTitleLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.totalAmountTitleLabel!)
         self.totalAmountTitleLabel?.snp.makeConstraints({ (make) -> Void in
-            make.bottom.equalTo(self.contentView.snp.centerY).offset(-5)
+            make.top.equalTo(self.contentView).offset(17)
             make.left.equalTo(self.contentView).offset(15)
         });
         
         self.totalAmountLabel = UILabel()
-        self.totalAmountLabel?.text = "71.6万元"
+        self.totalAmountLabel?.text = "710.6万元"
         self.totalAmountLabel?.font = XZClient.XZFont2(size: 18)
-        self.totalAmountLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.totalAmountLabel!)
         self.totalAmountLabel?.snp.makeConstraints({ (make) -> Void in
-            make.top.equalTo(self.contentView.snp.centerY).offset(5)
+            make.top.equalTo(self.contentView.snp.centerY).offset(6)
             make.left.equalTo(self.totalAmountTitleLabel!)
         });
         
         self.interestRateTitleLabel = UILabel()
         self.interestRateTitleLabel?.text = "利率"
         self.interestRateTitleLabel?.font = XZClient.XZFont2(size: 14)
-        self.interestRateTitleLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.interestRateTitleLabel!)
         self.interestRateTitleLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(self.totalAmountTitleLabel!)
-            make.left.equalTo((self.totalAmountTitleLabel?.snp.right)!).offset(15)
+            make.left.equalTo((self.totalAmountTitleLabel?.snp.right)!).offset(40)
         });
         
         self.interestRateLabel = UILabel()
         self.interestRateLabel?.text = "4.9%"
         self.interestRateLabel?.font = XZClient.XZFont2(size: 18)
-        self.interestRateLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.interestRateLabel!)
         self.interestRateLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(self.totalAmountLabel!)
@@ -203,19 +265,17 @@ class LoanDetails_DescribeTableViewCell: UITableViewCell {
         });
         
         self.periodTitleLabel = UILabel()
-        self.periodTitleLabel?.text = "利率"
+        self.periodTitleLabel?.text = "贷款年限"
         self.periodTitleLabel?.font = XZClient.XZFont2(size: 14)
-        self.periodTitleLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.periodTitleLabel!)
         self.periodTitleLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(self.totalAmountTitleLabel!)
-            make.left.equalTo((self.interestRateTitleLabel?.snp.right)!).offset(15)
+            make.left.equalTo((self.interestRateTitleLabel?.snp.right)!).offset(40)
         });
         
         self.periodLabel = UILabel()
-        self.periodLabel?.text = "4.9%"
+        self.periodLabel?.text = "30"
         self.periodLabel?.font = XZClient.XZFont2(size: 18)
-        self.periodLabel?.textColor = XZSwiftColor.xzGlay129
         self.contentView.addSubview(self.periodLabel!)
         self.periodLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(self.totalAmountLabel!)
@@ -241,47 +301,59 @@ class LoanDetails_CumulativeTableViewCell: UITableViewCell {
     }
     
     func sebView(){
-        self.contentView.backgroundColor = XZSwiftColor.convenientBackgroundColor
+        self.contentView.backgroundColor = XZSwiftColor.white
+        
+        let linView = UIView()
+        linView.backgroundColor = XZSwiftColor.xzGlay230
+        self.contentView.addSubview(linView)
+        linView.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(0.5);
+            make.left.top.right.equalTo(self.contentView)
+        }
         
         let label = UILabel()
         label.text = "累计利息（元）："
-        label.font = XZClient.XZFont2(size: 18)
-        label.textColor = XZSwiftColor.xzGlay129
+        label.font = XZClient.XZFont2(size: 15)
         self.contentView.addSubview(label)
         label.snp.makeConstraints({ (make) -> Void in
-            make.bottom.equalTo(self.contentView.snp.centerY).offset(-5)
-            make.left.equalTo(self.contentView)
+            make.top.equalTo(self.contentView).offset(15)
+            make.left.equalTo(self.contentView).offset(15)
         });
         
         self.totalAmountLabel = UILabel()
         self.totalAmountLabel?.text = "152432.00"
-        self.totalAmountLabel?.font = XZClient.XZFont2(size: 18)
-        self.totalAmountLabel?.textColor = XZSwiftColor.xzGlay129
+        self.totalAmountLabel?.font = XZClient.XZFont2(size: 15)
         self.contentView.addSubview(self.totalAmountLabel!)
         self.totalAmountLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(label)
-            make.left.equalTo(label).offset(5)
+            make.left.equalTo(label.snp.right).offset(5)
         });
         
         let totalLabel = UILabel()
         totalLabel.text = "累计还款金额（元）："
-        totalLabel.font = XZClient.XZFont2(size: 18)
-        totalLabel.textColor = XZSwiftColor.xzGlay129
+        totalLabel.font = XZClient.XZFont2(size: 15)
         self.contentView.addSubview(totalLabel)
         totalLabel.snp.makeConstraints({ (make) -> Void in
-            make.top.equalTo(self.contentView.snp.centerY).offset(5)
-            make.left.equalTo(self.contentView)
+            make.top.equalTo(label.snp.bottom).offset(15)
+            make.left.equalTo(self.contentView).offset(15)
         });
         
         self.interestRateLabel = UILabel()
         self.interestRateLabel?.text = "1245222.00"
-        self.interestRateLabel?.font = XZClient.XZFont2(size: 18)
-        self.interestRateLabel?.textColor = XZSwiftColor.xzGlay129
+        self.interestRateLabel?.font = XZClient.XZFont2(size: 15)
         self.contentView.addSubview(self.interestRateLabel!)
         self.interestRateLabel?.snp.makeConstraints({ (make) -> Void in
             make.bottom.equalTo(totalLabel)
-            make.left.equalTo(totalLabel).offset(5)
+            make.left.equalTo(totalLabel.snp.right).offset(5)
         });
+        
+        let linBView = UIView()
+        linBView.backgroundColor = XZSwiftColor.xzGlay230
+        self.contentView.addSubview(linBView)
+        linBView.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(0.5);
+            make.left.bottom.right.equalTo(self.contentView)
+        }
     }
 }
 
