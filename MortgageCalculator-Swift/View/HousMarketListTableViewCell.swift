cd959cc95afdbd940ce06e2a03b01fce875e8461
xuzhou524 <xuuohz@163.com>
2017-11-09 16:02:37 +0800

    bak

--- MortgageCalculator-Swift/View/HousMarketListTableViewCell.swift
+++ MortgageCalculator-Swift/View/HousMarketListTableViewCell.swift
@@ -70,7 +70,7 @@ class HousMarketListTableViewCell: ASCellNode {
         }
         
         if let sourceStr = self.digItem.source {
-            let attrStrings = NSMutableAttributedString(string: "来源：" + sourceStr,
+            let attrStrings = NSMutableAttributedString(string: "来源：" + "腾讯新闻",
                                                         attributes: [
                                                             NSForegroundColorAttributeName: XZSwiftColor.xzGlay129,
                                                             NSFontAttributeName:  XZClient.XZFont(size: 14)])
