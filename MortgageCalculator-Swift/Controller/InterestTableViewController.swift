845dd71422ced7ec276d67b8e2381ce1c0d8b8d9
xuzhou1 <xuzhou1@hualala.com>
2021-09-03 11:40:51 +0800

    bak

--- MortgageCalculator-Swift/Controller/InterestTableViewController.swift
+++ MortgageCalculator-Swift/Controller/InterestTableViewController.swift
@@ -13,26 +13,16 @@ class InterestTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "基准利率表"
-        self.view.backgroundColor = XZSwiftColor.convenientBackgroundColor
-        
-        let label = UILabel()
-        label.text = "中国人民银行2015年10月24日基准利率表"
-        label.font = XZClient.XZFont(size: 15)
-        label.textColor = XZSwiftColor.xzGlay50
-        view.addSubview(label)
-        label.snp.makeConstraints({ (make) in
-            make.top.equalToSuperview().offset(20)
-            make.left.equalToSuperview().offset(15)
-        })
+        self.view.backgroundColor = XZSwiftColor.white
         
         let shangyeImageView = UIImageView()
         shangyeImageView.image = UIImage.init(named: "shangye")
         view.addSubview(shangyeImageView)
         shangyeImageView.snp.makeConstraints({ (make) in
             make.left.equalToSuperview().offset(10)
-            make.top.equalTo(label.snp_bottom).offset(15)
-            make.width.equalTo(275 * 1.2)
-            make.height.equalTo(140 * 1.2)
+            make.top.equalToSuperview().offset(20)
+            make.width.equalTo(XZClient.ScreenWidth() - 20)
+            make.height.equalTo((XZClient.ScreenWidth() - 20) / 3)
         })
         
         let gongjijinImageView = UIImageView()
@@ -41,8 +31,8 @@ class InterestTableViewController: UIViewController {
         gongjijinImageView.snp.makeConstraints({ (make) in
             make.left.equalToSuperview().offset(10)
             make.top.equalTo(shangyeImageView.snp_bottom).offset(15)
-            make.width.equalTo(275 * 1.2)
-            make.height.equalTo(140 * 1.2)
+            make.width.equalTo(228 * 1.3)
+            make.height.equalTo(193 * 1.3)
         })
         
     }
